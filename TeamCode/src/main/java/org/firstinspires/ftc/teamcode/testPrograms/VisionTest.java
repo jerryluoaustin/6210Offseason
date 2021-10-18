ebb691ca7006f93fee267e6fff831eb8168c6891
jerry <jerryluoaustin@users.noreply.github.com>
2021-10-18 17:21:40 -0500

    Added vision color testing, check if pixel rgb blue value is less than 50 to get orange pixel.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testPrograms/VisionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testPrograms/VisionTest.java
@@ -1,5 +1,12 @@
 package org.firstinspires.ftc.teamcode.testPrograms;
 
+import static android.graphics.Color.blue;
+import static android.graphics.Color.green;
+import static android.graphics.Color.red;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -20,10 +27,28 @@ public class VisionTest extends LinearOpMode {
         //OpenCV openCV = new OpenCV(this);
         VuforiaBM vuforia = new VuforiaBM(this);
 
+
         waitForStart();
+
+        Bitmap bm = vuforia.getBitmap();
+
+        int pixel2 = bm.getPixel(640, 360);
+        int pixel1 = bm.getPixel(0, 450);
+        telemetry.addData("center pixel", pixel1);
+        telemetry.addData("left pixel", pixel2);
+
+        telemetry.addData("Lred", red(pixel1));
+        telemetry.addData("Lgreen", green(pixel1));
+        telemetry.addData("Lblue", blue(pixel1));
+
+        telemetry.addData("Rred", red(pixel2));
+        telemetry.addData("Rgreen", green(pixel2));
+        telemetry.addData("Rblue", blue(pixel2));
+
 
         telemetry.addData("program done",": complete");
         telemetry.update();
+        sleep(10000);
 
     }
 }
