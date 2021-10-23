f976b6bd0d833289c2b347341d8c712c65648f14
jerry <jerryluoaustin@users.noreply.github.com>
2021-10-23 13:48:20 -0500

    Added mecanum drive to tele-op

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name="TeleOpMode", group = "TeleOp")
 public class TeleOpMode extends OpMode
@@ -15,15 +16,20 @@ public class TeleOpMode extends OpMode
 
     public void init()
     {
-        FR = hardwareMap.dcMotor.get("FR");
-        FL = hardwareMap.dcMotor.get("FL");
-        BR = hardwareMap.dcMotor.get("BR");
-        BL = hardwareMap.dcMotor.get("BL");
+        FR = hardwareMap.dcMotor.get("rightFront");
+        FL = hardwareMap.dcMotor.get("leftFront");
+        BR = hardwareMap.dcMotor.get("rightRear");
+        BL = hardwareMap.dcMotor.get("leftRear");
 
         FR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         FL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         BR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         BL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        FR.setDirection(DcMotorSimple.Direction.REVERSE);
+        FL.setDirection(DcMotorSimple.Direction.FORWARD);
+        BR.setDirection(DcMotorSimple.Direction.REVERSE);
+        BL.setDirection(DcMotorSimple.Direction.FORWARD);
 
         telemetry.addData("init ", "completed");
         telemetry.update();
@@ -45,39 +51,40 @@ public class TeleOpMode extends OpMode
         BL.setPower(0);
     }
 
-    double flp = 0;
-    double frp = 0;
-    double blp = 0;
-    double brp = 0;
-
-
     @Override
     public void loop()
     {
+        double leftY = 0;
+        double leftX = 0;
+        double rightX = 0;
+        double[] motorPower = new double[4];
+
         if (Math.abs(gamepad1.left_stick_y) > 0.1)
         {
-            flp = gamepad1.left_stick_y;
-            frp = gamepad1.left_stick_y;
-            blp = gamepad1.left_stick_y;
-            brp = gamepad1.left_stick_y;
+            leftY = gamepad1.left_stick_y;
         }
 
-        if(gamepad1.right_stick_x > 0.1)
+        if (Math.abs(gamepad1.left_stick_x) > 0.1)
         {
-            flp += Math.abs(gamepad1.right_stick_x);
-            blp += Math.abs(gamepad1.right_stick_x);
+            leftX = gamepad1.left_stick_x;
         }
 
-        if(gamepad1.right_stick_x < -0.1)
+        if(Math.abs(gamepad1.right_stick_x) > 0.1)
         {
-            frp += Math.abs(gamepad1.right_stick_x);
-            brp += Math.abs(gamepad1.right_stick_x);
+            rightX = gamepad1.right_stick_x;
         }
 
-        FR.setPower(frp);
-        FL.setPower(flp);
-        BR.setPower(brp);
-        BL.setPower(blp);
+
+
+        motorPower[0] = leftY - leftX + rightX;
+        motorPower[1] = leftY - leftX - rightX;
+        motorPower[2] = leftY + leftX + rightX;
+        motorPower[3] = leftY + leftX - rightX;
+
+        FR.setPower(motorPower[0]);
+        FL.setPower(motorPower[1]);
+        BR.setPower(motorPower[2]);
+        BL.setPower(motorPower[3]);
 
     }
 
